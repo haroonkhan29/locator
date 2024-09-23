db8d3bf82bcb3906811427d8aafae626cdef2e60
haroonkhan29 <haroon77.afridi@gmail.com>
2024-09-23 20:50:58 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -6,29 +6,35 @@ import MainPage from './components/MainPage';
 import Lookup from './components/Lookup';
 import Tracker from './components/Tracker';
 import IsdCodes from './components/IsdCodes';
-// import PeopleSearch from './components/PeopleSearch';
 import History from './components/History';
 import Registration from './components/Registration';
 import Login from './components/Login';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
     const [isLoginButtonClicked, setLoginButtonClicked] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsLoggedIn(true);
+            setLoginButtonClicked(true);
+        }
+    }, []);
 
     return (
         <Router>
             <div className="App">
                 <Navbar isLoginButtonClicked={isLoginButtonClicked} />
                 <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/lookup" element={<Lookup />}/>
-                    <Route path="/ip-tracker" element={<Tracker />}/>
-                    <Route path="/isd-codes" element={<IsdCodes />}/>
-                    {/* <Route path="/people-search" element={<PeopleSearch />}/> */}
-                    <Route path="/history" element={<History/>}/>
+                    <Route path="/" element={isLoggedIn ? <MainPage /> : <Navigate to="/login" />} />
+                    <Route path="/lookup" element={isLoggedIn ? <Lookup /> : <Navigate to="/login" />} />
+                    <Route path="/ip-tracker" element={isLoggedIn ? <Tracker /> : <Navigate to="/login" />} />
+                    <Route path="/isd-codes" element={isLoggedIn ? <IsdCodes /> : <Navigate to="/login" />} />
+                    <Route path="/history" element={isLoggedIn ? <History /> : <Navigate to="/login" />} />
                     <Route path="/register" element={<Registration />} />
                     <Route path="/login" element={<Login setLoginButtonClicked={setLoginButtonClicked} />} />
-             
                 </Routes>
                 <Footer />
             </div>
